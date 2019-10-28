8abe519c156ef642dc6adffe7ee8733e8dac4206
Michael Lin <zxlin@sfu.ca>
2019-10-27 23:23:05 -0700

    added audiokit and unwind segue code
    need to fix audiokit version, figure out how to store audiokit on github

---
+++ Health Bars/Health Bars/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  Health Bars
+//
+//  Created by Michael Lin on 2019-10-27.
+//  Copyright © 2019 Michael Lin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Health Bars/Health Bars/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  Health Bars
+//
+//  Created by Michael Lin on 2019-10-26.
+//  Copyright © 2019 Michael Lin. All rights reserved.
+//
+
+import AudioKit
+import AudioKitUI
+
+import UIKit
+
+class ViewController: UIViewController {
+    //@IBOutlet var plot: AKNodeOutputPlot?
+
+    var oscillator1 = AKOscillator()
+    var oscillator2 = AKOscillator()
+    var mixer = AKMixer()
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        mixer = AKMixer(oscillator1, oscillator2)
+
+        // Cut the volume in half since we have two oscillators
+        mixer.volume = 0.5
+        AudioKit.output = mixer
+        do {
+            try AudioKit.start()
+        } catch {
+            AKLog("AudioKit did not start!")
+        }
+    }
+
+    @IBAction func toggleSound(_ sender: UIButton) {
+        if oscillator1.isPlaying {
+            oscillator1.stop()
+            oscillator2.stop()
+            sender.setTitle("Play Sine Waves", for: .normal)
+        } else {
+            oscillator1.frequency = random(in: 220 ... 880)
+            oscillator1.start()
+            oscillator2.frequency = random(in: 220 ... 880)
+            oscillator2.start()
+            sender.setTitle("Stop \(Int(oscillator1.frequency))Hz & \(Int(oscillator2.frequency))Hz", for: .normal)
+        }
+    }
+
+    @IBAction func unwindToStart(_ unwindSegue: UIStoryboardSegue) {
+        let sourceViewController = unwindSegue.source
+        // Use data from the view controller which initiated the unwind segue
+    }
+    
+}
+
