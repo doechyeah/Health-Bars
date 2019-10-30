9c27b9491d31d69e90a28f8c584e97d8b1fc2283
Michael Lin <zxlin@sfu.ca>
2019-10-30 12:19:04 -0700

    cleaned up sections of code, more cleanup to be done

--- Health Bars/Health Bars/ViewController.swift
+++ Health Bars/Health Bars/ViewController.swift
@@ -6,59 +6,69 @@
 //  Copyright © 2019 Michael Lin. All rights reserved.
 //
 
+// Majority of AudioKit code in this file is taken from AudioKit examples Hello World and Microphone Analysis
+
 import AudioKit
 import AudioKitUI
 
 import UIKit
 
 class ViewController: UIViewController {
-    //@IBOutlet var plot: AKNodeOutputPlot?
     
+    //MARK: Constants
     let noteFrequencies = [16.35, 17.32, 18.35, 19.45, 20.6, 21.83, 23.12, 24.5, 25.96, 27.5, 29.14, 30.87]
     let noteNamesWithSharps = ["C", "C♯", "D", "D♯", "E", "F", "F♯", "G", "G♯", "A", "A♯", "B"]
     let noteNamesWithFlats = ["C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭", "A", "B♭", "B"]
     
     
-    //@IBOutlet weak var frequencyLabel: UILabel!
-    //@IBOutlet weak var noteNameWithSharpsLabel: UILabel!
-    //@IBOutlet weak var noteNameWithFlatsLabel: UILabel!
-    //@IBOutlet weak var amplitudeLabel: UILabel!
+    //MARK: Outlets
     @IBOutlet weak var frequencyLabel: UILabel!
     @IBOutlet weak var amplitudeLabel: UILabel!
     @IBOutlet weak var noteNameWithSharpsLabel: UILabel!
     @IBOutlet weak var noteNameWithFlatsLabel: UILabel!
     
     
-    
+    //MARK: Condition variables
     var success: Bool = false
     
-    var oscillator1 = AKOscillator()
-    var mixer = AKMixer()
-    //var oscillator2 = AKOscillator()
+    //MARK: AudioKit variables
+    var oscillator1: AKOscillator!
+    var mixer: AKMixer!
+    
     var mic: AKMicrophone!
     var tracker: AKFrequencyTracker!
     var bandpassfilter: AKBandPassButterworthFilter!
     var silence: AKBooster!
     
+    // called when view first gets loaded into memory
     override func viewDidLoad() {
+        // debug
+        NSLog("viewDidLoad")
         super.viewDidLoad()
-        
-        
     }
     
+    // called when view appears fully
     override func viewDidAppear(_ animated: Bool) {
+        // debug
+        NSLog("viewDidAppear")
         super.viewDidAppear(animated)
         
         AKSettings.audioInputEnabled = true
+        // workaround for bug in audiokit: https://github.com/AudioKit/AudioKit/issues/1799#issuecomment-506373157
         AKSettings.sampleRate = AudioKit.engine.inputNode.inputFormat(forBus: 0).sampleRate
-        mic = AKMicrophone()
-        bandpassfilter = AKBandPassButterworthFilter(mic)
-        bandpassfilter.centerFrequency = 800
-        bandpassfilter.bandwidth = 700
-        tracker = AKFrequencyTracker(bandpassfilter)
-        //have to give the frequencytracker an output, or else it won't work
-        silence = AKBooster(tracker, gain: 0)
         
+        oscillator1 = AKOscillator()
+        mixer = AKMixer()
+        
+        mic = AKMicrophone()
+        // filter out non-vocal frequencies
+        bandpassfilter = AKBandPassButterworthFilter(mic, centerFrequency: 800, bandwidth: 750)
+        tracker = AKFrequencyTracker(bandpassfilter)
+        // have to connect the frequencytracker to an output, or else it won't work
+        silence = AKBooster(tracker, gain: 0)
+        mixer = AKMixer(oscillator1, silence)
+        
+        // set mic input to first (may not be necessary)
         if let inputs = AudioKit.inputDevices {
             do {
                 try AudioKit.setInputDevice(inputs[0])
@@ -70,7 +80,6 @@ class ViewController: UIViewController {
             AKLog("failed to get mic")
         }
         
-        mixer = AKMixer(oscillator1, silence)
         
         // Cut the volume in half since we have two oscillators
         mixer.volume = 0.5
@@ -85,17 +94,28 @@ class ViewController: UIViewController {
                              selector: #selector(ViewController.updateUI),
                              userInfo: nil,
                              repeats: true)
+        
+        // debug
+        NSLog("Done viewDidAppear")
+        
     }
     
+    // called with view disappears fully
     override func viewDidDisappear(_ animated: Bool) {
+        // debug
+        NSLog("viewDidDisappear")
+        
         do {
             try AudioKit.stop()
         } catch {
             AKLog("AudioKit did not stop!")
         }
+        
+        // debug
+        NSLog("Done viewDidDisappear")
     }
     
-    
+    // start/stop sine oscillator
     @IBAction func toggleSound(_ sender: UIButton) {
         if oscillator1.isPlaying {
             oscillator1.stop()
@@ -110,11 +130,13 @@ class ViewController: UIViewController {
         }
     }
     
+    // unwind segue function, called from other views
     @IBAction func unwindToStart(_ unwindSegue: UIStoryboardSegue) {
         //let sourceViewController = unwindSegue.source
         // Use data from the view controller which initiated the unwind segue
     }
     
+    // action segue to conditionally go to second view
     @IBAction func gotoSecondView(_ sender: UIButton) {
         if success == false {
             performSegue(withIdentifier: "ID_gotoSecondView", sender: self)
@@ -122,6 +144,9 @@ class ViewController: UIViewController {
     }
     
     @objc func updateUI() {
+        // debug
+        NSLog("updateUI")
+        
         if tracker.amplitude > 0.1 {
             frequencyLabel.text = String(format: "%0.1f", tracker.frequency)
             
