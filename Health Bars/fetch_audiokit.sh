49f523c0b0a88fa806471b4f98260d2b569d3e81
doechyeah <dsong.chan@gmail.com>
2019-11-15 14:10:43 -0800

    Updated the fetch framework script

--- Health Bars/fetch_audiokit.sh
+++ Health Bars/fetch_audiokit.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 cd "${1}"
-my_deps="AudioKit.framework"
-if [ -d "${my_deps}" -o -L "${my_deps}"  ]; then
-  echo "AudioKit.framework already exists, skipping fetch"
+my_depSQL="SQLite.swift-0.12.2"
+my_depAudio="AudioKit.framework"
+
+if [ -d "${my_depAudio}" -o -L "${my_depAudio}"  ]; then
+  echo "AudioKit already exists, skipping fetch"
 else
   echo "Fetching AudioKit.framework"
   curl -O -L https://github.com/AudioKit/AudioKit/releases/download/v4.7.2/AudioKit-iOS-4.7.2.zip
@@ -11,5 +13,16 @@ else
   echo "Done fetching AudioKit.framework, cleaning up downloaded files"
   rm -rf AudioKit-iOS-4.7.2.zip
   rm -rf AudioKit-iOS
+  echo "Done cleanup of downloaded files"
+fi
+
+if [ -d "${my_depSQL}" -o -L "${my_depSQL}"  ]; then
+  echo "SQLite.swift already exists, skipping fetch"
+else
+  echo "Fetching SQLite.swift.Framework"
+  curl -O -L https://github.com/stephencelis/SQLite.swift/archive/0.12.2.zip
+  unzip 0.12.2.zip
+  echo "Done fetching SQLite.framework, cleaning up downloaded files"
+  rm -rf 0.12.2.zip
   echo "Done cleanup of downloaded files"
 fi
