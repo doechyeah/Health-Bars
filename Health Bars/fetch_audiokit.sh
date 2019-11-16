d5faa7c19da7fcedc7717675246d221d8ac6cb02
Michael Lin <zxlin@sfu.ca>
2019-11-15 20:02:45 -0800

    remove redundant sqlite.swift folder and fetch_audiokit.sh lines

--- Health Bars/fetch_audiokit.sh
+++ Health Bars/fetch_audiokit.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 cd "${1}"
-my_depSQL="SQLite.swift-0.12.2"
 my_depAudio="AudioKit.framework"
 
 if [ -d "${my_depAudio}" -o -L "${my_depAudio}"  ]; then
@@ -13,16 +12,5 @@ else
   echo "Done fetching AudioKit.framework, cleaning up downloaded files"
   rm -rf AudioKit-iOS-4.7.2.zip
   rm -rf AudioKit-iOS
-  echo "Done cleanup of downloaded files"
-fi
-
-if [ -d "${my_depSQL}" -o -L "${my_depSQL}"  ]; then
-  echo "SQLite.swift already exists, skipping fetch"
-else
-  echo "Fetching SQLite.swift.Framework"
-  curl -O -L https://github.com/stephencelis/SQLite.swift/archive/0.12.2.zip
-  unzip 0.12.2.zip
-  echo "Done fetching SQLite.framework, cleaning up downloaded files"
-  rm -rf 0.12.2.zip
   echo "Done cleanup of downloaded files"
 fi
