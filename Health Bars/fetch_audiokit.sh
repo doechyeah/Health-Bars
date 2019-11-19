b984de0a55de36e9984ad454beba35e315746e5f
Daniel Song <esa46@asb9700-e04.csil.sfu.ca>
2019-11-18 16:02:05 -0800

    update fetch_audiokit.sh to fetch audiokit 4.9.1 for xcode 11.1

--- Health Bars/fetch_audiokit.sh
+++ Health Bars/fetch_audiokit.sh
@@ -6,11 +6,11 @@ if [ -d "${my_depAudio}" -o -L "${my_depAudio}"  ]; then
   echo "AudioKit already exists, skipping fetch"
 else
   echo "Fetching AudioKit.framework"
-  curl -O -L https://github.com/AudioKit/AudioKit/releases/download/v4.7.2/AudioKit-iOS-4.7.2.zip
-  unzip AudioKit-iOS-4.7.2.zip
+  curl -O -L https://github.com/AudioKit/AudioKit/releases/download/v4.9.1/AudioKit-iOS-4.9.1.zip
+  unzip AudioKit-iOS-4.9.1.zip
   cp -a AudioKit-iOS/AudioKit.framework .
   echo "Done fetching AudioKit.framework, cleaning up downloaded files"
-  rm -rf AudioKit-iOS-4.7.2.zip
+  rm -rf AudioKit-iOS-4.9.1.zip
   rm -rf AudioKit-iOS
   echo "Done cleanup of downloaded files"
 fi
