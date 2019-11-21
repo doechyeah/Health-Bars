cc1296bbaf976d38def630826d82741e301eb017
Michael Lin <zxlin@sfu.ca>
2019-11-21 12:56:09 -0800

    start of moving audiokit into its own singleton class

---
+++ Health Bars/Health Bars/AudioKitConductor.swift
@@ -0,0 +1,32 @@
+//  Health Bars
+//
+//  Team: Team Rhythm
+//
+//  AudioKitConductor.swift
+//  AudioKit singleton class to manage all AudioKit objects
+//  Audiokit does not like dynamic memory management of its objects
+//
+//  Developers:
+//  Michael Lin
+//
+//  Copyright © 2019 Team Rhythm. All rights reserved.
+//
+//  Changelog:
+//  2019-11-20: Created
+//
+
+import AudioKit
+
+class AudioKitConductor {
+    static let sharedInstance = AudioKitConductor()
+    
+    var
+    
+    var
+    
+    init() {
+        
+    }
+    
+    
+}
